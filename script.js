8a37b601b35504130a92db590824dc6cbd314344
Nvlahogiannis12 <526Nvlahogiannis@frhsd.com>
2025-07-14 19:39:48 +0000

    done for today

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let playerHandValue;
 let opponentHandValue;
+let swappingHand = false;
+let newPlayerLeft;
+let newPlayerRight;
 //Objects For Player Hands
 let playerHand = {
   Left:1,
@@ -12,13 +15,18 @@ let oppHand = {
 };
 UpdateHands()
 function UpdateHands(){
+  if (swappingHand === true){
+  document.getElementById('playerLeft').innerText = newPlayerLeft;
+  document.getElementById('playerRight').innerText = newPlayerRight;
+  } else {
   document.getElementById('playerLeft').innerText = playerHand.Left;
   document.getElementById('playerRight').innerText = playerHand.Right;
   document.getElementById('oppLeft').innerText = oppHand.Left;
   document.getElementById('oppRight').innerText = oppHand.Right;
+  }
 }
 //when Tap is clicked adds event listener to the player hand for onclick
-function tapping(){
+function playerTapping(){
  document.getElementById("playLButton").onclick = () => {
     calcPlayerHand("playLButton");
   };
@@ -62,9 +70,66 @@ function calcOpponentHand(selectedOpHand){
 }
 
 function calc(){
-  //Calculates the new total. % 5 keeps it between a range of 0 - 4 so when hits 5 itll be 0
+  //Calculates the new total. % 5 keeps it between a range of 0 - 4 so when hits 5 it'll be 0
   const total = (opponentHandValue + playerHandValue) % 5;
   oppHand[selectedOpponentHandKey] = total;
   alert(total);
   UpdateHands();
 }
+
+/* SPLIT CODE */
+function playerSplitting(direction) {
+  if (!swappingHand) {
+    swappingHand = true;
+    newPlayerLeft = playerHand.Left;
+    newPlayerRight = playerHand.Right;
+  }
+
+  if (direction === "leftPMove") {
+    if (newPlayerRight - 1 >= 0 && newPlayerLeft + 1 < 5) {
+      newPlayerLeft++;
+      newPlayerRight--;
+      UpdateHands();
+    }
+    return;
+  }
+
+  if (direction === "rightPMove") {
+    if (newPlayerLeft - 1 >= 0 && newPlayerRight + 1 < 5) {
+      newPlayerLeft--;
+      newPlayerRight++;
+      UpdateHands();
+    }
+    return;
+  }
+
+  if (direction === "confirm") {
+    const sameAsBefore = 
+    (newPlayerLeft === playerHand.Left && newPlayerRight === playerHand.Right);
+
+    if (sameAsBefore) {
+      alert("Invalid split");
+      return;
+    }
+    // Apply split
+    playerHand.Left = newPlayerLeft;
+    playerHand.Right = newPlayerRight;
+    swappingHand = false;
+
+    alert("Split confirmed!");
+    UpdateHands();
+    return;
+  }
+  if (direction === "cancel") {
+    swappingHand = false;
+    alert("Split canceled.");
+    UpdateHands();
+    return;
+  }
+}
+/*
+VS COMPUTER STRAT TO CODE
+if Hand >= 5 after attack go for that hand
+If not attack smallest number unless it can kill you
+if 1 hand 2 more than the other than swap hands
+*/
