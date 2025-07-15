9095ac96261515b4a641733ded83156f5dd61855
Nvlahogiannis12 <526Nvlahogiannis@frhsd.com>
2025-07-15 14:49:34 +0000

    starting bot coding

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ let oppHand = {
   Left: 1,
   Right: 1,
 };
+
 UpdateHands()
 function UpdateHands(){
   if (swappingHand === true){
@@ -27,12 +28,15 @@ function UpdateHands(){
 }
 //when Tap is clicked adds event listener to the player hand for onclick
 function playerTapping(){
+  document.getElementById('playerActions').classList.add('d-none')
+  document.getElementById('playerTappingActions').classList.remove('d-none')
+
  document.getElementById("playLButton").onclick = () => {
-    calcPlayerHand("playLButton");
+    if (playerHand.Left){calcPlayerHand("playLButton")};
   };
 
   document.getElementById("playRButton").onclick = () => {
-    calcPlayerHand("playRButton");
+    if (playerHand.Right){calcPlayerHand("playRButton")};
   };
 }
 //adds selected class turns off event listener
@@ -48,12 +52,25 @@ function calcPlayerHand(selectedHand){
   alert(playerHandValue);
 
   document.getElementById("opLButton").onclick = () => {
-    calcOpponentHand("opLButton");
+    if (oppHand.Left){calcOpponentHand("opLButton")};
   };
 
   document.getElementById("opRButton").onclick = () => {
-    calcOpponentHand("opRButton");
+    if (oppHand.Right){calcOpponentHand("opRButton")};
   };
+}
+//Cancel tapping action
+function cancelTapp(){
+  document.getElementById('playerActions').classList.remove('d-none')
+  document.getElementById('playerTappingActions').classList.add('d-none')
+  //Removes all things with selectedPH class 
+  document.querySelectorAll('.selectedPH').forEach((el) => {
+    el.classList.remove('selectedPH');
+  });
+  document.getElementById("playLButton").onclick = null;
+  document.getElementById("playRButton").onclick = null;
+  document.getElementById("opLButton").onclick = null;
+  document.getElementById("opRButton").onclick = null;
 }
 
 let selectedOpponentHandKey;
@@ -75,10 +92,21 @@ function calc(){
   oppHand[selectedOpponentHandKey] = total;
   alert(total);
   UpdateHands();
+  document.getElementById('playerActions').classList.remove('d-none')
+  document.getElementById('playerTappingActions').classList.add('d-none')
+  //Removes all things with selectedPH class 
+  document.querySelectorAll('.selectedPH').forEach((el) => {
+    el.classList.remove('selectedPH');
+  });
+turnSwitch('opponent')
 }
 
 /* SPLIT CODE */
 function playerSplitting(direction) {
+  document.getElementById('playerActions').classList.add('d-none')
+  document.getElementById('playerSwapActions').classList.remove('d-none')
+  document.getElementById('playerSwaps').classList.remove('d-none')
+
   if (!swappingHand) {
     swappingHand = true;
     newPlayerLeft = playerHand.Left;
@@ -116,20 +144,41 @@ function playerSplitting(direction) {
     playerHand.Right = newPlayerRight;
     swappingHand = false;
 
-    alert("Split confirmed!");
+  document.getElementById('playerActions').classList.remove('d-none')
+  document.getElementById('playerSwapActions').classList.add('d-none')
+  document.getElementById('playerSwaps').classList.add('d-none')
     UpdateHands();
+    turnSwitch('opponent')
     return;
   }
   if (direction === "cancel") {
     swappingHand = false;
-    alert("Split canceled.");
+  document.getElementById('playerActions').classList.remove('d-none')
+  document.getElementById('playerSwapActions').classList.add('d-none')
+  document.getElementById('playerSwaps').classList.add('d-none')
     UpdateHands();
     return;
   }
 }
+
 /*
 VS COMPUTER STRAT TO CODE
 if Hand >= 5 after attack go for that hand
 If not attack smallest number unless it can kill you
 if 1 hand 2 more than the other than swap hands
+if any of the player hands can kill then swap
 */
+
+/*TURN SWITCH*/
+function turnSwitch(turn){
+  if (turn == 'opponent'){
+    document.getElementById('playerActions').classList.add('d-none')
+    opponentComputing()
+  } else if (turn == 'player'){
+    document.getElementById('playerActions').classList.remove('d-none')
+  }
+}
+
+function opponentComputing(){
+  if(oppHand.Left + playerHand.Left >= 5 || oppHand.Left + playerHand.Right >= 5 || oppHand.Right + playerHand.Left >= 5 || oppHand.Right + playerHand.Right >= 5){}
+}
